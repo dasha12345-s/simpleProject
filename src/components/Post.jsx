c5bde3630bf3118e0f65661bc9ea2c5e1849773e
Darya <shostak19@icloud.com>
2024-05-16 11:22:27 -0400

    Outputting Dynamic Values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,12 @@
+const names = ['Darya', 'Fed' ]
+
 export default function Post(){
+
+  const dynamicName = Math.random() > 0.5 ? names[0] : names[1];
+
   return(
   <div>
-    <p>Daria</p>
+    <p>{dynamicName}</p>
     <p>First post</p>
   </div>
   )
