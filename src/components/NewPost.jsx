4102e9bbafd8ce173f0fe43479ef2c6839d77e3f
Darya <shostak19@icloud.com>
2024-05-16 14:03:50 -0400

    Lifting State Up

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,25 +1,16 @@
 import classes from './NewPost.module.css';
-import { useState } from 'react';
 
-function NewPost() {
-
-  const [enteredText, setEnteredText] = useState('');
-
-
-  function changeHandler(event){
-    setEnteredText(event.target.value);
-  }
+function NewPost(props) {
 
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeHandler}/>
+        <textarea id="body" required rows={3} onChange={props.onTextChange}/>
       </p>
-      <p>{enteredText}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,15 +1,26 @@
 import classes from './PostList.module.css'
 import Post from "./Post";
 import NewPost from "./NewPost.jsx";
+import { useState } from 'react';
 
 export default function PostList(props){
+  const [enteredText, setEnteredText] = useState('');
+  const [author, setAuthor] = useState('');
+  
+  function changeHandler(event){
+    setEnteredText(event.target.value);
+  }
+
+  function changeAuthorHandler(event){
+    setAuthor(event.target.value);
+  }
+
   return(
     <>
-    <NewPost/>
-
+    <NewPost onTextChange={changeHandler} onAuthorChange={changeAuthorHandler}/>
     <ul className={classes.posts}>
-      <Post author='Daria' body='me'/>
-      <Post author='Fed' body='he'/>
+      <Post author={author} body={enteredText}/>
+      <Post author={author} body={enteredText}/>
     </ul>
     </>   
   )
