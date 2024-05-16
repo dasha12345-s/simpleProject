a4e8c4663f2f4f243899dd7f1de3163614e6ad84
Darya <shostak19@icloud.com>
2024-05-16 13:24:23 -0400

    Preparing the App For State Management

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
 import classes from './PostList.module.css'
 import Post from "./Post";
+import NewPost from "./NewPost.jsx";
 
 export default function PostList(props){
   return(
+    <>
+    <NewPost/>
+
     <ul className={classes.posts}>
       <Post author='Daria' body='me'/>
       <Post author='Fed' body='he'/>
     </ul>
-   
+    </>   
   )
 }
