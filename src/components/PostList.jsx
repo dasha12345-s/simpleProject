56b634082ff9bd3c8409f7905aa9a5282fe7920a
Darya <shostak19@icloud.com>
2024-05-17 19:05:17 -0400

    Handle Loading State

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -7,12 +7,15 @@ import { useEffect, useState } from 'react';
 export default function PostList({isVisible, onStopShowing}){
 
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
     async function fetchPost(){
+      setIsFetching(true);
       const response = await fetch('http://localhost:8080/posts')
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPost();
@@ -38,15 +41,20 @@ export default function PostList({isVisible, onStopShowing}){
         onCancel={onStopShowing} onAddPost={addPostHandler}/>
          </Modal>
     )}
-    {posts.length > 0 && (
+    {!isFetching && posts.length > 0 &&(
       <ul className={classes.posts}>
       {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}/> )}
     </ul>
     )}
-    {posts.length === 0 && (
+    {!isFetching && posts.length === 0 && (
       <div style={{ textAlign: 'center', color: 'white' }}>
         <h2>There are no props yet.</h2>
         <p>Start adding some!</p>
+      </div>
+    )}
+    {isFetching && (
+      <div style={{textAlign: 'center', color: 'white'}}>
+        <p>Loading posts...</p>
       </div>
     )}
     </>   
